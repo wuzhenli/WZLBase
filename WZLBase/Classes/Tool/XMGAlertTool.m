ee50896147ce56ae6e7ee732b847b99a7873e0d5
Jaly <jaly2012@sina.cn>
2019-03-02 23:06:04 +0800

    modify alert tool

--- WZLBase/Classes/Tool/XMGAlertTool.m
+++ WZLBase/Classes/Tool/XMGAlertTool.m
@@ -12,27 +12,27 @@
 @implementation XMGAlertTool
 
 
-+ (void)alertTitle:(NSString *)titile type:(UIAlertControllerStyle)alertType message:(NSString *)message didTask:(void(^)())task {
-    
-    
-    UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:titile message:message preferredStyle:alertType];
-    
-    UIAlertAction *action = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
-
-    }];
-    
-    UIAlertAction *action1 = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        if (task) {
-            task();
-        }
-    }];
-    
-    [alertVC addAction:action];
-    [alertVC addAction:action1];
-    
-    
-    [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alertVC animated:YES completion:nil];
-}
+//+ (void)alertTitle:(NSString *)titile type:(UIAlertControllerStyle)alertType message:(NSString *)message didTask:(void(^)())task {
+//    
+//    
+//    UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:titile message:message preferredStyle:alertType];
+//    
+//    UIAlertAction *action = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+//
+//    }];
+//    
+//    UIAlertAction *action1 = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+//        if (task) {
+//            task();
+//        }
+//    }];
+//    
+//    [alertVC addAction:action];
+//    [alertVC addAction:action1];
+//    
+//    
+//    [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alertVC animated:YES completion:nil];
+//}
 
 
 @end
